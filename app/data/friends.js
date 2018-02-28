7ca4e60fb3bd9dc16ec8aa8039b2e511ada930c8
hillary-joyce <hillary.j.scofield@gmail.com>
2018-02-27 22:19:51 -0500

    fixed logic for comparing dogs

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,6 +17,38 @@ var dogData = [
       4,
       1
     ]
+  },
+  {
+    "name":"Spot",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+      4,
+      2,
+      4,
+      3,
+      4,
+      1,
+      1,
+      1,
+      3,
+      4
+    ]
+  },
+  {
+  "name":"Lando",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      1,
+      2,
+      3,
+      4,
+      5,
+      4,
+      3,
+      2,
+      1,
+      2
+    ]
 }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,25 +11,37 @@ module.exports = function(app) {
   //Second route is a POST route, used to handle incoming survey results
   //Also will be used to handle compatabilty logic
   app.post("/api/friends", function(req, res){
-
+    console.log(req.body);
     //create a variable called "currentBestMatch"
     //this will be equal to the dog object that is currently the best currentBestMatch
+    var currentBestMatch = "";
     //create a variable called "bestMatchDifference"
     //this will be equal to the match difference for the current best dog match
-
+    var bestMatchDifference = 1000;
     //loop through all other dogs in the dogArray
+    for(i = 0; i < dogArray.length; i++){
+      var diff = 0;
     //compare the difference between current user's scores against those from other users, question by question.
+      for(j = 0; j < 10; j++){
+        addToDiff = Math.abs(dogArray[i].scores[j] - parseInt(req.body.scores[j]));
+        diff = diff + addToDiff;
+      }
+    console.log(diff);
     //Add up the differences to calculate the `totalDifference`.
     //if the totalDifference is less than the bestMatchDifference
+    if(diff < bestMatchDifference){
     //set bestMatchDifference to totalDifference and currentBestMatch to the current dog object
+      currentBestMatch = dogArray[i];
+      bestMatchDifference = diff;
+    }
 
-
+    }
     //Display friend result as a modal popup
 
     //Then add the new dog to the dogArray
     dogArray.push(req.body);
-    res.json("Data Added", dogArray);
+    res.json(currentBestMatch);
 
-    console.log();
+    console.log(parseInt(req.body.scores[2]));
   })
 }
